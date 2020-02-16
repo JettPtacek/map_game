6c693da618e3e1c9a7b1635e160d4f834a771332
Jett Ptacek <jett.ptacek@gmail.com>
2020-02-16 17:36:26 -0500

    created simple structs and 2d grid with terrians

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
-struct Map {
-    grid: [[u8; 4]; 4],
-    number: u8,
+struct MapToken{
+    id :u8,
+    class: u8,
 }
+
+impl Copy for MapToken {}
+
+impl Clone for MapToken {
+    fn clone(&self) -> MapToken {
+        *self
+    }
+}
+struct Map {
+    id          :u8,
+    terrian_grid : [[ u8; 10] ; 10],
+    entity_grid  : [[ MapToken; 10 ]; 10],
+}
+
+/**
+ * 2d map creation
+ */
 
 
 fn main() {
-    let array = [[2; 4];4];
-    let map1 = Map {
-        grid: array,
-        number: 4,
+    let no_entity = MapToken{
+        id :1,
+        class :0
     };
-
-    println!("Array {} Number {}", map1.grid[0][0] , map1.number);
+    let entity_grid = [[no_entity; 10]; 10];
+    let terrian_grid = [[2; 10];10];
+    let map1 = Map {
+        id: 0,
+        terrian_grid: terrian_grid,
+        entity_grid: entity_grid,
+    };
+    println!("Map ID: {}", map1.id);
+    println!("entity grid id [0][0] {}", map1.entity_grid[0][0].id);
+    println!("entity grid class [0][0] {}", map1.entity_grid[0][0].class);
+    println!("terrian grid number [0][0] {}", map1.terrian_grid[0][0]);
 }
